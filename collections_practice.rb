821a4dfd1345658c8d953ae4abee5e8340626510
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-17 22:44:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,6 +24,8 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-    array.each do |item| item[2] = "$" #
+    array.each do |item| item[2] = "$" 
+    # Think of a string (word) such as "Kesha" as an array.  "K" is at index 0, "E" is at index 1 and so on.  You can access these individual letters inside a string just like you could with elements in an array by calling the index once you assign the string a variable.
   end 
 end
+
